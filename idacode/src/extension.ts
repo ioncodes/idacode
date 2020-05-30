c8f38fd64a5c219fd0442386e3df5be84e681c54
Layle <luca@marcelli.ch>
2020-05-30 20:33:41 +0200

    tabs bad

--- idacode/src/extension.ts
+++ idacode/src/extension.ts
@@ -3,28 +3,28 @@ import * as net from 'net';
 import * as path from 'path';
 
 export function activate(context: vscode.ExtensionContext) {
-	let disposable = vscode.commands.registerCommand('idacode.executeScript', () => {
-		const scriptPath = vscode.window.activeTextEditor?.document.uri.fsPath as string;
-		const config = vscode.workspace.getConfiguration('IDACode');
-		const host = config.get('host') as string;
-		const port = config.get('port') as number;
+    let disposable = vscode.commands.registerCommand('idacode.executeScript', () => {
+        const scriptPath = vscode.window.activeTextEditor?.document.uri.fsPath as string;
+        const config = vscode.workspace.getConfiguration('IDACode');
+        const host = config.get('host') as string;
+        const port = config.get('port') as number;
 
-		if (scriptPath !== undefined) {
-			const name = path.parse(scriptPath).base;
-			const client = new net.Socket();
-			
-			client.on('error', _ => {
-				vscode.window.showErrorMessage(`Failed sending ${name} to IDA`);
-			});
+        if (scriptPath !== undefined) {
+            const name = path.parse(scriptPath).base;
+            const client = new net.Socket();
+            
+            client.on('error', _ => {
+                vscode.window.showErrorMessage(`Failed sending ${name} to IDA`);
+            });
 
-			client.connect(port, host, () => {
-				client.write(scriptPath);
-				vscode.window.showInformationMessage(`Sent ${name} to IDA`);
-			});
-		}
-	});
+            client.connect(port, host, () => {
+                client.write(scriptPath);
+                vscode.window.showInformationMessage(`Sent ${name} to IDA`);
+            });
+        }
+    });
 
-	context.subscriptions.push(disposable);
+    context.subscriptions.push(disposable);
 }
 
 export function deactivate() {}
