1790e8c16c1ed3e4711ae5cf8999f6b078c8da44
Layle <luca@marcelli.ch>
2020-05-31 14:43:18 +0200

    refactored ida plugin

--- ida/idacode.py
+++ ida/idacode.py
@@ -13,6 +13,7 @@ import idacode_utils.dbg as dbg
 
 HOST = "127.0.0.1"
 PORT = 7065
+DEBUG_PORT = 7066
 PYTHON = "C:\\Python37\\python37.exe"
 
 getcwd_original = os.getcwd
@@ -25,35 +26,53 @@ def getcwd_hook():
         cwd = script_folder
     return cwd
 
-def start_server():
+def setup_patches():
     os.getcwd = getcwd_hook
     sys.executable = PYTHON
-    debugpy.listen(7066)
 
+def start_debug_server():
+    debugpy.listen(DEBUG_PORT)
+    print(f"IDACode debug server listening on {HOST}:{DEBUG_PORT}")
+
+def start_socket_server():
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     sock.bind((HOST, PORT))
     sock.listen(5)
     print(f"IDACode listening on {HOST}:{PORT}")
+    return sock
+
+def create_env():
+    return {
+        "idaapi": SafeIDAAPI(),
+        "idc": SafeIDC(),
+        "idautils": SafeIDAUtils(),
+        "dbg": dbg,
+        "idacode": True
+    }
+
+def handle_connection(script):
+    global script_folder
+    
+    if script and len(script) > 0:
+        script = script.decode("utf8")
+        script_folder = os.path.dirname(script)
+        env = create_env()
+        print(f"Executing {script}")
+        # TODO: try wrapping IDAPython_ExecScript in a safe handler instead
+        idaapi.IDAPython_ExecScript(script, env)
+
+def start_server():
+    setup_patches()
+    sock = start_socket_server()
+    start_debug_server()
 
     while True:
         io, _ = sock.accept()
-        script = io.recv(256) # MAX_PATH
+        data = io.recv(256) # MAX_PATH
         io.close()
 
-        if script and len(script) > 0:
-            script = script.decode("utf8")
-            print(f"Executing {script}")
-            vars = {
-                "idaapi": SafeIDAAPI(),
-                "idc": SafeIDC(),
-                "idautils": SafeIDAUtils(),
-                "dbg": dbg,
-                "idacode": True
-            }
-            # TODO: try wrapping IDAPython_ExecScript in a safe handler instead
-            global script_folder
-            script_folder = os.path.dirname(script)
-            idaapi.IDAPython_ExecScript(script, vars)
+        handle_connection(data)
+
 
 class VSCodeServer(idaapi.plugin_t):
     def __init__(self):
